cbd441e9e223226f9da82cb1aabbf6fc30aed5b9
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-05-13 17:27:13 +0400

    add user card

--- src/pages/Home/components/SearchForm/index.tsx
+++ src/pages/Home/components/SearchForm/index.tsx
@@ -1,27 +1,26 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useRef } from 'react';
 
 interface Props {
   onSearch: (value: string) => void;
 }
 
 const SearchForm = ({ onSearch }: Props): JSX.Element => {
-  const [value, setValue] = useState('');
+  const inp = useRef<HTMLInputElement>(null);
 
   //   TODO instead of submit can add debounce search
   const handleSubmit = (event: FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
 
-    onSearch(value);
-
-    setValue('');
+    if (inp.current) {
+      onSearch(inp.current.value);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="mb-4">
       <input
+        ref={inp}
         type="text"
-        value={value}
-        onChange={(e) => setValue(e.target.value)}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       />
     </form>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,18 +2,20 @@ import { useContext } from 'react';
 
 import { GitHubContext } from '../../context/github';
 
+import UserCard from './components/UserCard';
 import SearchForm from './components/SearchForm';
 
 const Home = (): JSX.Element => {
-  const context = useContext(GitHubContext);
-
-  if (!context) {
-    return <div>no context...</div>;
-  }
+  const { data, handleSearch } = useContext(GitHubContext);
 
   return (
     <>
-      <SearchForm onSearch={context.handleSearch} />
+      <SearchForm onSearch={handleSearch} />
+      <div className="grid grid-cols-4 gap-4 pb-4">
+        {data.map((item) => (
+          <UserCard key={item.id} data={item} />
+        ))}
+      </div>
     </>
   );
 };
